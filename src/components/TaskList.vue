9416c4e87490c999a109c6a2569fa23c4356bd26
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-02-08 23:21:25 +0700

    Created a TaskList component to Todo page

---
+++ src/components/TaskList.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul>
+        <li>
+            <div class="task-container">
+                <input type="checkbox">
+                <label>Task 1</label>
+                <button class="delete">Delete</button>
+            </div>
+        </li>
+<!--        <ul class="task">Task 2</ul>-->
+    </ul>
+</template>
+
+<script>
+
+    export default {
+        name: "TaskList"
+    }
+</script>
+
+<style scoped>
+    .task {
+        background-color: white;
+        width: 100%;
+    }
+</style>
