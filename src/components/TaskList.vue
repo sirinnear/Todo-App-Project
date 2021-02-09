7a4e21ba13fa632739bfad3bb5a56c9479a07483
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-02-09 13:14:52 +0700

    Customized CSS for task items

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,11 +1,14 @@
 <template>
-    <ul>
+    <ul class="todo-list">
         <li>
-            <div class="task-container">
-                <input type="checkbox">
-                <label>Task 1</label>
-                <button class="delete">Delete</button>
-            </div>
+            <input type="checkbox">
+            <label>Task 1</label>
+            <button class="delete">Delete</button>
+        </li>
+        <li>
+            <input type="checkbox">
+            <label>Task 2</label>
+            <button class="delete">Delete</button>
         </li>
 <!--        <ul class="task">Task 2</ul>-->
     </ul>
@@ -19,8 +22,41 @@
 </script>
 
 <style scoped>
-    .task {
+    .todo-list {
+
+        list-style: none;
+        margin: 10px 0px;
+        padding: 0px;
+    }
+
+    .todo-list li {
+        position: relative;
         background-color: white;
-        width: 100%;
+        margin: 5px;
+        border-radius: 5px;
+        /*height: 20px;*/
+        padding: 10px;
+    }
+
+    .todo-list input[type=checkbox] {
+        text-align: center;
+        width: 40px;
+        position: absolute;
+    }
+
+    .todo-list label {
+        text-align: center;
+        word-break: break-all;
+        line-height: 1.2;
+        /*width: 200px;*/
+        /*padding: 15px 15px 15px 60px;*/
+        display: block;
+    }
+
+    .todo-list .delete {
+        position: absolute;
+        width: 40px;
+        right: 10px;
+        top: 9px;
     }
 </style>
