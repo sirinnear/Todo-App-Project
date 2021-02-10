a3f5e6410f13c58299c594b9d3564eaf4657c650
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-02-10 23:27:01 +0700

    Vuetified Login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,22 +1,52 @@
 <template>
-    <div class="outer-container">
-        <div class="inner-container">
-            <h1>Login</h1>
-            <div class="input-container">
-                <label>Email</label>
-                <br/>
-                <input type="text">
-            </div>
-            <div class="input-container">
-                <label>Password</label>
-                <br/>
-                <input type="password">
-            </div>
-            <button class="button">Login</button>
-            <button class="button" style="margin: 0px">Register</button>
-            <p id="placeholder"></p>
-        </div>
-    </div>
+<!--    <div class="outer-container">-->
+<!--        <div class="inner-container">-->
+<!--            <h1>Login</h1>-->
+<!--            <div class="input-container">-->
+<!--                <label>Email</label>-->
+<!--                <br/>-->
+<!--                <input type="text">-->
+<!--            </div>-->
+<!--            <div class="input-container">-->
+<!--                <label>Password</label>-->
+<!--                <br/>-->
+<!--                <input type="password">-->
+<!--            </div>-->
+<!--            <button class="button">Login</button>-->
+<!--            <button class="button" style="margin: 0px">Register</button>-->
+<!--            <p id="placeholder"></p>-->
+<!--        </div>-->
+<!--    </div>-->
+    <v-app>
+        <v-container>
+            <v-card
+            outlined
+            class="mt-16 mx-auto elevation-3 rounded-lg"
+            max-width="500"
+            min-width="300">
+                <v-card-title>Login</v-card-title>
+                <v-form>
+                    <v-container>
+                        <v-text-field
+                                dense
+                                outlined
+                                label="Email"
+                                class="rounded-xl"></v-text-field>
+                        <v-text-field
+                                dense
+                                outlined
+                                label="Password"
+                                :type="'password'"
+                                class="rounded-xl"></v-text-field>
+                        <v-btn block dark class="green lighten-2 rounded-xl">Login with Email</v-btn>
+                        <v-btn block dark class="blue lighten-1 mt-2 rounded-xl">Login with Google</v-btn>
+                        <!--                        <v-btn dark class="blue lighten-2 ml-2">Register</v-btn>-->
+                    </v-container>
+
+                </v-form>
+            </v-card>
+        </v-container>
+    </v-app>
 </template>
 
 <script>
@@ -26,13 +56,16 @@
 </script>
 
 <style scoped>
-    h1 {
-        text-align: center;
+    #app {
+        background-color: #E3F2FD;
     }
+    /*h1 {*/
+    /*    text-align: center;*/
+    /*}*/
 
-    .input-container {
-        margin: 10px 30px;
-    }
+    /*.input-container {*/
+    /*    margin: 10px 30px;*/
+    /*}*/
 
     /*.input-container input {*/
     /*    box-sizing: border-box;*/
@@ -45,20 +78,20 @@
     /*    outline: none;*/
     /*}*/
 
-    .input-container input, label {
-        padding: 10px;
-        margin: 10px 0px;
-        font-size: inherit;
-    }
+    /*.input-container input, label {*/
+    /*    padding: 10px;*/
+    /*    margin: 10px 0px;*/
+    /*    font-size: inherit;*/
+    /*}*/
 
-    button {
-        padding: 10px;
-        margin: 0px 10px 20px 30px;
-        font-size: inherit;
-        font-family: inherit;
-        color: white;
-        background-color: rgba(110, 112, 178, 0.51);
-        border: rgba(99, 99, 99, 0.39) solid 2px;
-        border-radius: 5px;
-    }
+    /*button {*/
+    /*    padding: 10px;*/
+    /*    margin: 0px 10px 20px 30px;*/
+    /*    font-size: inherit;*/
+    /*    font-family: inherit;*/
+    /*    color: white;*/
+    /*    background-color: rgba(110, 112, 178, 0.51);*/
+    /*    border: rgba(99, 99, 99, 0.39) solid 2px;*/
+    /*    border-radius: 5px;*/
+    /*}*/
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from '@/router';
 import store from '@/store';
 
-import '@/assets/style.css';
+// import '@/assets/style.css';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
