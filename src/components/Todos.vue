f27cedf1dafb6394e76f8df7ea6d74c6d602352e
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-02-12 23:09:54 +0700

    Done basic header function to Todo page

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,11 +1,4 @@
 <template>
-<!--    <div class="outer-container">-->
-<!--        <div class="inner-container">-->
-<!--            <h1>Todos</h1>-->
-<!--            <input type="text" placeholder="What are you going to do?">-->
-<!--            <TaskList/>-->
-<!--        </div>-->
-<!--    </div>-->
     <v-app>
         <v-app-bar class="blue lighten-2" flat app>
             <v-spacer></v-spacer>
@@ -16,25 +9,34 @@
         </v-app-bar>
         <v-main>
             <v-card
-            min-width="300"
-            max-width="600"
-            class="mx-auto mt-15 rounded-xl">
-                <v-container fluid>
-                    <v-text-field
-                    solo
-                    label="What will you be doing?"
-                    class="mt-3 rounded-xl"></v-text-field>
-                    <v-row class="mx-1 mt-n4">
-                        <v-col>
-                            <p class="mt-2">0 item(s) left</p>
-                        </v-col>
-                        <v-col>
-                            <p class="mt-2">0 item(s) completed</p>
-                        </v-col>
-                        <v-col>
-                            <v-btn>Clear completed</v-btn>
-                        </v-col>
+                min-width="600"
+                max-width="600"
+                class="mx-auto mt-10 px-4 rounded-xl">
+                <v-container>
+                        <v-text-field
+                                outlined
+                                rounded
+                                label="What are you going to do?"
+                                class="mt-3 mb-n5"></v-text-field>
+                        <v-row
+                                class="text-center pl-4 mt-2">
+                            <v-col cols="3">
+                                <strong>0 item(s) left</strong>
+                            </v-col>
+                            <v-col cols="4">
+                                <strong>0 item(s) completed</strong>
+                            </v-col>
+                            <v-col cols="5" class="mt-n5">
+                                <v-switch
+                                        class="mr-2"
+                                        label="Hide completed"></v-switch>
+                            </v-col>
+                        </v-row>
+                    <v-row>
+                        <v-btn
+                        class="mx-auto mt-n2 mb-4">Clear completed</v-btn>
                     </v-row>
+                    <TaskList/>
                 </v-container>
             </v-card>
         </v-main>
@@ -44,9 +46,10 @@
 <script>
     // import TaskList from '@/components/TaskList';
 
+    import TaskList from "@/components/TaskList";
     export default {
         name: "Todo",
-        components: {},
+        components: {TaskList},
         data() {
             return {
 
